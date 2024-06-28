10296cf746bdf345761928fd523c32c3c7a97c88
Ajith-beehyv <ajith.k@beehyv.com>
2024-06-28 13:05:25 +0530

    adding Service annotation again

--- src/main/java/org/egov/web/notification/sms/service/SMSService.java
+++ src/main/java/org/egov/web/notification/sms/service/SMSService.java
@@ -1,7 +1,9 @@
 package org.egov.web.notification.sms.service;
 
 import org.egov.web.notification.sms.models.Sms;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface SMSService {
     void sendSMS(Sms sms);
 }
