3ac516c61a62e4d4a3bb6636386b0bd162fd0861
Ajith-beehyv <ajith.k@beehyv.com>
2024-06-28 11:22:02 +0530

    Revert "add Servcie annotation in SMSServcie class"
    This reverts commit a7458eb78c6c1bf484012cad4748b1918a176bfa.

--- src/main/java/org/egov/web/notification/sms/service/SMSService.java
+++ src/main/java/org/egov/web/notification/sms/service/SMSService.java
@@ -1,9 +1,7 @@
 package org.egov.web.notification.sms.service;
 
 import org.egov.web.notification.sms.models.Sms;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface SMSService {
     void sendSMS(Sms sms);
 }
